eae7e3cff3d3b6a13f405af9336ea85b60cc2732
Mikko <mikefulli@gmail.com>
2018-12-13 00:56:56 +0200

    Added guided random search to day 11 part 2

--- 11/fuel_max.py
+++ 11/fuel_max.py
@@ -1,7 +1,7 @@
 from time import time
 from functools import lru_cache
 from itertools import product
-from multiprocessing import Pool
+from random import randint
 
 serial_number = 1788
 
@@ -17,7 +17,7 @@ def calculate_power(x, y):
 
 def calculate_area(coordinates):
 	if coordinates[0] + coordinates[2] > 300 or coordinates[1] + coordinates[2] > 300:
-		return None
+		return -999999
 	levels = []
 	for x, y in product(range(0, coordinates[2]), repeat=2):
 		levels.append(calculate_power(coordinates[0] + x, coordinates[1] + y))
@@ -26,16 +26,59 @@ def calculate_area(coordinates):
 if __name__ == '__main__':
 	start_time = time()
 
-	# Calculate list of power levels
-	process_pool = Pool(None)
-	coordinates = list(product(range(1, 299), range(1, 299), range(1, 299)))
-	coordinate_levels = process_pool.map(calculate_area, coordinates)
+	largest_level = -99999
+	largest_solution = [randint(1,300), randint(1,300), randint(1,300)]
+	level = -99999
+	current_solution = [randint(1,300), randint(1,300), randint(1,300)]
+	change_counter = 0
+	reset_counter = 0
+	solution_time = time()
 
-	# Get largest power level and get coordinates for it
-	largest_level = max(coordinate_levels)
-	larget_index = coordinate_levels.index(largest_level)
-	larget_coordinates = list(coordinates)[larget_index]
+	while level < 1000:
+		change_counter += 1
 
-	print(larget_coordinates)
+		# Swap two tasks in solution and evaluate it
+		new_x = current_solution[0] + randint(-15, 15)
+		new_y = current_solution[1] + randint(-15, 15)
+		new_size = current_solution[2] + randint(-15, 15)
+		if new_x < 1:
+			new_x = 1
+		if new_x > 300:
+			new_x = 300
+		if new_y < 1:
+			new_y = 1
+		if new_y > 300:
+			new_y = 300
+		if new_size < 1:
+			new_size = 1
+		if new_size > 300:
+			new_size = 300
+
+		current_solution = [new_x, new_y, new_size]
+		level = calculate_area(current_solution)
+
+		# Check if better solution was found
+		if level > largest_level:
+			largest_solution = current_solution.copy()
+			largest_level = level
+			change_counter = 0
+			reset_counter = 0
+			solution_time = time()
+
+		# Reset current solution to last good solution after too many tries
+		if change_counter > 3000:
+			change_counter = 0
+			reset_counter += 1
+			current_solution = largest_solution.copy()
+
+		# Retry from start if solving takes too long
+		if time() - solution_time > 30:
+			print(largest_solution, largest_level)
+			largest_level = -99999
+			largest_solution = [randint(1,300), randint(1,300), randint(1,300)]
+			change_counter = 0
+			reset_counter = 0
+			solution_time = time()
+			current_solution = [randint(1,300), randint(1,300), randint(1,300)]
 
 	print('Finished in', time() - start_time)
